b72a84be10513f2fe50532a3ad86a4e6b907d15c
Charles Sternbach <csternbach@gmail.com>
2026-09-13 15:10:29 -0400

    Section 1.02 stacking dollar bills to match Sears tower height problem

---
+++ Work/sears.py
@@ -0,0 +1,31 @@
+""" 
+Author: David Beazley
+Date 9-13-26
+Purpose: Solve the following practice problem and understand the given solution
+"""
+
+""" 
+Problem Statement:
+    One morning, you go out and place a dollar bill on the sidewalk by the 
+    Sears tower in Chicago. Each day thereafter, you go out double 
+    the number of bills. How long does it take for the stack of bills to exceed 
+    the height of the tower?
+    
+    Extra note: Each day thereafter you double the number of bills that were 
+        there on the previous day.
+"""
+
+# Here is one possible solution:
+bill_thickness = 0.11 * 0.001 # Meters (0.11 mm)
+sears_height = 442 # Height (meters)
+num_bills = 1
+day = 1
+
+while num_bills * bill_thickness < sears_height:
+    print(day, num_bills, num_bills * bill_thickness)
+    day = day + 1
+    num_bills = num_bills * 2
+
+print('Number of days', day)
+print('Number of bills', num_bills)
+print('Final height', num_bills * bill_thickness)
